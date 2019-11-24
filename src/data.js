edd94c9daa0ef4fefbc18e646163daec53978554
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-24 15:11:06 +0100

    added data map

---
+++ src/data.js
@@ -0,0 +1,26 @@
+import mud from "./Assets/mud-hunt.png";
+import treasure from "./Assets/lambda-treasure-hunt.png";
+
+const data = [
+    {
+        image: mud,
+        title: "Mud Hunt",
+        github: "https://github.com/orgs/mud-hunt/dashboard",
+        deployed: "https://mudhunt.netlify.com/"
+    },
+    {
+        image: treasure,
+        title: "Treasure Hunt",
+        github: "https://github.com/CSEU2-BW2"
+    },
+    {
+        image: "https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif",
+        title: "Card 3"
+    },
+    {
+        image: "https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif",
+        title: "Card 4"
+    }
+];
+
+export default data;
