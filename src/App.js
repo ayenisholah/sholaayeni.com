fde9660ffe9b6f59d4b619f48a07ed1ce029cda7
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-24 17:45:58 +0100

    contact form

--- src/App.js
+++ src/App.js
@@ -10,43 +10,43 @@ import Footer from "./Components/Footer";
 import Success from "./Components/success";
 
 const darkModeSettings = {
-    color: `rgb(233, 233, 233)`,
-    backgroundColor: "rgb(23, 23, 35"
+  color: `rgb(233, 233, 233)`,
+  backgroundColor: "rgb(23, 23, 35"
 };
 
 function App() {
-    const [darkMode, setDarkMode] = useState(false);
-    const darkStyle = darkMode ? darkModeSettings : null;
+  const [darkMode, setDarkMode] = useState(false);
+  const darkStyle = darkMode ? darkModeSettings : null;
 
-    useEffect(() => {
-        const isDark = localStorage.getItem("dark-mode")
-            ? localStorage.getItem("dark-mode")
-            : false;
-        setDarkMode(JSON.parse(isDark));
-    }, []);
-    return (
-        <div style={darkStyle} className="App">
-            <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-            <Route
-                exact
-                path="/"
-                render={props => <About {...props} darkMode={darkMode} />}
-            />
-            <Route
-                path="/portfolio"
-                render={props => <Portfolio {...props} darkMode={darkMode} />}
-            />
-            <Route
-                path="/contact"
-                render={props => <Contact {...props} darkMode={darkMode} />}
-            />
-            <Route
-                path="/success"
-                render={props => <Success {...props} darkMode={darkMode} />}
-            />
-            <Footer darkMode={darkMode} setDarkMode={setDarkMode} />
-        </div>
-    );
+  useEffect(() => {
+    const isDark = localStorage.getItem("dark-mode")
+      ? localStorage.getItem("dark-mode")
+      : false;
+    setDarkMode(JSON.parse(isDark));
+  }, []);
+  return (
+    <div style={darkStyle} className="App">
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Route
+        exact
+        path="/"
+        render={props => <About {...props} darkMode={darkMode} />}
+      />
+      <Route
+        path="/portfolio"
+        render={props => <Portfolio {...props} darkMode={darkMode} />}
+      />
+      <Route
+        path="/contact"
+        render={props => <Contact {...props} darkMode={darkMode} />}
+      />
+      <Route
+        path="/success"
+        render={props => <Success {...props} darkMode={darkMode} />}
+      />
+      <Footer darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
+  );
 }
 
 export default App;
